d453c5ab07a51a35ca05eeb3b813f0d0326a8884
Voytovych <voytovych.ua@gmail.com>
2016-06-09 20:55:20 +0300

    Simple junit test

--- ProteinTrackerTests/src/TrackingServiceTests.java
+++ ProteinTrackerTests/src/TrackingServiceTests.java
@@ -1,20 +1,35 @@
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import ua.voytovych.junit.TrackingService;
-import static org.junit.Assert.*;
 
 public class TrackingServiceTests {
 	
+	private TrackingService service;
+	
+	@Before
+	public void setUp(){
+		System.out.println("Before");
+		service = new TrackingService();
+	}
+
 	@Test
-	public void NewTrackingServiceTotalIsZero(){
-		TrackingService service = new TrackingService();
+	public void newTrackingServiceTotalIsZero(){
 		assertEquals("Tracking service total was not zero", 0, service.getTotal());
 	}
 	
 	@Test
-	public void WhenAddingProteinTotalIncreasesByThatAmount(){
-		TrackingService service = new TrackingService();
+	public void whenAddingProteinTotalIncreasesByThatAmount(){
 		service.addProtein(10);
 		assertEquals("Protein amount was not correct", 10, service.getTotal());
+	}
+	
+	@Test
+	public void whenRemovingProteinTotalRemainsZero(){
+		service.removeProtein(5);
+		assertEquals(0, service.getTotal());
 	}
 }
